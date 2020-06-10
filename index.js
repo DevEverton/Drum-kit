958c3d1d5ed9e0e1d4d46d95367480b8de1b07be
Everton Carneiro <Everton@MacBook-Pro-de-Everton.local>
2020-06-10 18:08:56 -0300

    Refactoring entire code and add keypress eventlistener

--- index.js
+++ index.js
@@ -1,76 +1,51 @@
 window.addEventListener("load", () => {
-  setButtonsImg();
   handleClick();
+  handleKeyPress();
 });
 
-function getButtons() {
-  const wBtn = document.querySelector(".w");
-  const aBtn = document.querySelector(".a");
-  const sBtn = document.querySelector(".s");
-  const dBtn = document.querySelector(".d");
-  const jBtn = document.querySelector(".j");
-  const kBtn = document.querySelector(".k");
-  const lBtn = document.querySelector(".l");
-  return [wBtn, aBtn, sBtn, dBtn, jBtn, kBtn, lBtn];
+function handleClick() {
+  const buttons = document.querySelectorAll(".drum");
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      makeSound(event.target.textContent);
+    });
+  });
+}
+function handleKeyPress() {
+  document.addEventListener("keydown", (event) => {
+    makeSound(event.key);
+  });
 }
 
-function setButtonsImg() {
-  const imgs = [
-    "tom1.png",
-    "tom2.png",
-    "tom3.png",
-    "tom4.png",
-    "kick.png",
-    "snare.png",
-    "crash.png",
-  ];
-  const buttons = getButtons();
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].style.backgroundImage = `url(./images/${imgs[i]})`;
+function makeSound(event) {
+  switch (event) {
+    case "w":
+      play("./sounds/tom-1.mp3");
+      break;
+    case "a":
+      play("./sounds/tom-2.mp3");
+      break;
+    case "s":
+      play("./sounds/tom-3.mp3");
+      break;
+    case "d":
+      play("./sounds/tom-4.mp3");
+      break;
+    case "j":
+      play("./sounds/kick-bass.mp3");
+      break;
+    case "k":
+      play("./sounds/snare.mp3");
+      break;
+    case "l":
+      play("./sounds/crash.mp3");
+      break;
+    default:
+      break;
   }
 }
 
-function drums() {
-  const tom1 = new Audio("./sounds/tom-1.mp3");
-  const tom2 = new Audio("./sounds/tom-2.mp3");
-  const tom3 = new Audio("./sounds/tom-3.mp3");
-  const tom4 = new Audio("./sounds/tom-4.mp3");
-  const kickBass = new Audio("./sounds/kick-bass.mp3");
-  const snare = new Audio("./sounds/snare.mp3");
-  const crash = new Audio("./sounds/crash.mp3");
-  return { tom1, tom2, tom3, tom4, kickBass, snare, crash };
-}
-
-function handleClick() {
-  const buttons = getButtons();
-  buttons.forEach((button) => {
-    button.addEventListener("mouseover", (event) => {
-      switch (event.target.textContent) {
-        case "w":
-          drums().tom1.play();
-
-          break;
-        case "a":
-          drums().tom2.play();
-          break;
-        case "s":
-          drums().tom3.play();
-          break;
-        case "d":
-          drums().tom4.play();
-          break;
-        case "j":
-          drums().kickBass.play();
-          break;
-        case "k":
-          drums().snare.play();
-          break;
-        case "l":
-          drums().crash.play();
-          break;
-        default:
-          break;
-      }
-    });
-  });
+function play(location) {
+  let sound = new Audio(location);
+  sound.play();
 }
